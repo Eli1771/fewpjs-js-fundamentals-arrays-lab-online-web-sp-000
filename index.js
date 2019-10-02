1462c0f02b9d473937423168ef252b88bd52974c
elijahmm17 <elijahmm17@gmail.com>
2019-10-02 20:31:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const prepend = ["Milo", "Otis", "Garfield"];
 const removeLast = ["Milo", "Otis", "Garfield"];
 const removeFirst = ["Milo", "Otis", "Garfield"];
 
+append.push("Odie");
+append.unshift("Odie");
+removeLast.pop();
+removeFirst.shift();
